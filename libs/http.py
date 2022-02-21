900b375b13d972d86de32a8c27253abda8c2e95e
Rob Nanola <robnanola@gmail.com>
2022-02-21 16:54:55 +0800

    Added custom http headers

--- libs/http.py
+++ libs/http.py
@@ -93,9 +93,29 @@ class Request:
         """
         base_url = self.base_url.format(endpoint=endpoint)
         endpoint = endpoint.lower()
+
+        # adding some custom headers as for some reason without this
+        # endpoint request sometime fails
+        headers = {
+            'Host': 'stats.nba.com',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+        }
+
         self.parameters = parameters
 
-        response = requests.get(url=base_url, params=parameters, timeout=30)
+
+        response = requests.get(
+            url=base_url,
+            params=parameters,
+            headers=headers,
+            timeout=30
+        )
         
         url = response.url
         status_code = response.status_code
